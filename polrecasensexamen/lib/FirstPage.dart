1f6670765d4bf9d7ab563a99ddbb1a7ffe480cc4
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2020-01-15 23:27:58 +0100

    send to firebase valors

--- polrecasensexamen/lib/FirstPage.dart
+++ polrecasensexamen/lib/FirstPage.dart
@@ -11,31 +11,43 @@ class _FirstPageState extends State<FirstPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("FireBase"),
-        ),
-        body: StreamBuilder(
-          stream: Firestore.instance.collection('Tareas').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            List<DocumentSnapshot> docs = snapshot.data.documents;
-            return ListView.builder(
-              itemCount: docs.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> data = docs[index].data;
-                return ListTile(
-                  leading: Checkbox(
-                    value: data['done'],
-                  ),
-                  title: Text(data['what']),
-                );
-              },
+        title: Text("FireBase"),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('Tareas').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ),
+          }
+          List<DocumentSnapshot> docs = snapshot.data.documents;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data = docs[index].data;
+              return ListTile(
+                leading: Checkbox(
+                  value: data['done'],
+                  onChanged: (bool valor) {
+                    setState(() {
+                      setInfoDB(valor, docs[index].documentID, data);
+                    });
+                  },
+                ),
+                title: Text(data['what']),
+              );
+            },
+          );
+        },
+      ),
     );
+  }
+
+  void setInfoDB(bool valor, String id, Map<String, dynamic> data) async {
+    await Firestore.instance
+        .collection('Tareas')
+        .document(id)
+        .setData({'done': valor, 'what': data['what']});
   }
 }
